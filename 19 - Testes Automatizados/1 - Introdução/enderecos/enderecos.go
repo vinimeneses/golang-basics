0404f71e49420d529039951aa6bf2ddaf525236e
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-01-19 15:21:42 -0300

    Testes avançados

--- 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos.go
+++ 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// TipoDeEndereco verifica se um endereço tem um tipo válido e o retorna
 func TipoDeEndereco(endereco string) string {
 	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
 
@@ -14,9 +15,8 @@ func TipoDeEndereco(endereco string) string {
 
 	enderecoTemUmTipoValido := false
 	for _, tipo := range tiposValidos {
-		if strings.Contains(primeiraPalavraDoEndereco, tipo) {
+		if tipo == primeiraPalavraDoEndereco {
 			enderecoTemUmTipoValido = true
-			break
 		}
 	}
 
@@ -25,5 +25,5 @@ func TipoDeEndereco(endereco string) string {
 		return caser.String(primeiraPalavraDoEndereco)
 	}
 
-	return "Tipo inválido"
+	return "Tipo Inválido"
 }
---
+++ 19 - Testes Automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -0,0 +1,31 @@
+package enderecos
+
+import "testing"
+
+type cenarioDeTest struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+func TestTipoDeEndereco(t *testing.T) {
+	cenariosDeTeste := []cenarioDeTest{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo Inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		retornoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				retornoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
+	}
+}
