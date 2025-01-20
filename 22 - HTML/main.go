9768291a05b5128d04b5bcec52e7f19f28d593ac
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-01-20 13:58:23 -0300

    html e css

---
+++ 22 - HTML/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates *template.Template
+
+type Usuario struct {
+	Nome  string
+	Email string
+}
+
+func main() {
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		u := Usuario{"Fulano", "fulano@gmail.com"}
+		err := templates.ExecuteTemplate(w, "home.html", u)
+		if err != nil {
+			return
+		}
+	})
+
+	fmt.Println("Server is running at :5000")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
