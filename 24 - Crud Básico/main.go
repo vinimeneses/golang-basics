58ffe8ec6523fb001436ce647208eb2315b02cf8
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-01-23 14:40:28 -0300

    implement getUser() function

--- 24 - Crud Básico/main.go
+++ 24 - Crud Básico/main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuarios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuario).Methods(http.MethodGet)
 
 	fmt.Println("Ouvindo na porta :5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
--- 24 - Crud Básico/servidor/servidor.go
+++ 24 - Crud Básico/servidor/servidor.go
@@ -57,3 +57,53 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
 }
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar ao banco de dados!"))
+	}
+	defer db.Close()
+
+	rows, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao consultar o banco de dados!"))
+		return
+	}
+	defer rows.Close()
+
+	var usuarios []usuario
+	for rows.Next() {
+		var usuario usuario
+
+		if erro := rows.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário"))
+			return
+		}
+		usuarios = append(usuarios, usuario)
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	// TESTANDO ESCREVER O BODY DO HTTP NUM ARQUIVO .TXT
+	//arquivo, err := os.OpenFile("JsonEncoder.txt", os.O_WRONLY|os.O_TRUNC, 0644)
+	//if err != nil {
+	//	fmt.Println("Erro ao abrir arquivo:", err)
+	//	return
+	//}
+	//defer arquivo.Close() // Garante o fechamento do arquivo
+	//
+	//if erro := json.NewEncoder(arquivo).Encode(usuarios); erro != nil {
+	//	w.Write([]byte("Erro ao converter usuário para JSON"))
+	//	return
+	//}
+
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converter usuário para JSON!"))
+		return
+	}
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
